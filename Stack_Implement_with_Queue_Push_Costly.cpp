384958e78519b89b9a4fd000b8e739b8054062a2
Armaan Singh Dhaliwal <117513972+Armaan-Dhaliwal@users.noreply.github.com>
2023-06-28 12:24:57 +0530

    Add files via upload

---
+++ Stack_Implement_with_Queue_Push_Costly.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<stack>
+#include<queue>
+
+using namespace std;
+
+class MyStack {
+public:
+    queue<int> q1;
+    queue<int> q2;
+
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q2.push(x);
+        while(!q1.empty()){
+            int temp;
+            temp=q1.front();
+            q1.pop();
+            q2.push(temp);
+        }
+
+        while(!q2.empty()){
+            int temp;
+            temp=q2.front();
+            q2.pop();
+            q1.push(temp);
+        }
+    
+    }
+    
+    int pop() {
+        int temp=q1.front();
+        q1.pop();
+        return temp;
+    }
+    
+    int top() {
+        return q1.front();
+    }
+    
+    bool empty() {
+        if(q1.empty()){
+            return true;
+        }
+
+            return false;
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
